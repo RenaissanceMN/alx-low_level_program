c0b026cdb7688e2ed7c29485f8ee10be14172560
RenaissanceMN <emekeic+githubgmail.com>
2022-10-21 00:44:47 +0100

    cgit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
-#i*nclude "holberton.h"
+#include "holberton.h"
 
-/*
- * _isupper - checks if parameter is an uppercase character.
- *   * @c: input character.
- *    * Return: 1 if is an uppercase character, 0 in other case.
- *     */
+/**
+ *_isupper - checks if parameter is an uppercase character.
+ *@c: input character.
+ *Return: 1 if is an uppercase character, 0 in other case.
+ */
 int _isupper(int c)
 {
 		if (c >= 65 && c <= 90)
